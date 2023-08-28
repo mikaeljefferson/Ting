cf6630fb0526a57b241ddc17d89918f6c5cc318c
mikael <mikaeljeffeson@gmail.com>
2023-08-28 15:38:48 -0300

    Feat: Req 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,24 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
+
+    def is_empty(self):
+        return self.data == []
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+       if not self.is_empty():
+            return self.data.pop(0)
+       else:
+            raise IndexError("Fila vazia!")
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index not in range(len(self.data)):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.data[index]
