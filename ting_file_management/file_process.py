596104fafea0202d4d1536c5441e4536e19e1e8d
mikael <mikaeljeffeson@gmail.com>
2023-08-29 15:44:29 -0300

    Feat: req 03,04,05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,37 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        arquivo_info = instance.search(index)
+        if arquivo_info.get("nome_do_arquivo") == path_file:
+            print(f"Arquivo {path_file} já foi processado anteriormente.")
+            return
+
+    lines = txt_importer(path_file)
+
+    arquivo_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(arquivo_info)
+
+    print(arquivo_info)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.is_empty():
+        return print("Não há elementos")
+
+    index = instance.dequeue()
+    return print(f"Arquivo {index['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position < 0 or position >= len(instance):
+        sys.stderr.write("Posição inválida\n")
+    else:
+        sys.stdout.write(str(instance.search(position)))
