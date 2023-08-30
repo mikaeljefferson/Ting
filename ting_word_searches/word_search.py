a4e9859ed9be06990c581c2ff1ab58850adb7939
mikael <mikaeljeffeson@gmail.com>
2023-08-30 09:16:05 -0300

    Feat: req 007

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,15 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    for data in instance.data:
+        find_word = search_by_word(word, data["linhas_do_arquivo"])
+        if find_word:
+            result = {
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias":  find_word
+            }
+            results.append(result)
+    return results
 
 
 def search_by_word(word, instance):
